e36ef91c1af936b79c03ad4ecfabcdb887cffe11
dbr2586 <109696856+dbr2586@users.noreply.github.com>
2022-09-23 19:13:32 +0200

    updates

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function getColors(){
     hexCodes = []
     rootColor = document.getElementById("eye-dropper").value
     document.body.style.background = rootColor
+    document.getElementById("root-color-name").value = rootColor
     hexCodes.push(rootColor)
     rootColor = document.getElementById("eye-dropper").value.slice(1, 7)
     mode = document.getElementById("mode-menu").value 
@@ -52,12 +53,21 @@ function getColors(){
             document.getElementById(`color${i}`).style.background = value
             document.getElementById(`color${i}-name`).innerHTML = `<span id="span${i}">${value}<span>`
             document.getElementById(`span${i}`).style.background = value
+            let red = parseInt(value.slice(1,3), 16)
+            let green = parseInt(value.slice(3,5), 16)
+            let blue = parseInt(value.slice(4,6), 16)
+            function adjustColors(){
+              (red*0.299 + green*0.587 + blue*0.114) > 186 ? document.getElementById(`span${i}`).style.color = "#000000" : document.getElementById(`span${i}`).style.color = "#ffffff"
+            }
+            adjustColors()
     }})}
 
     for (let i = 1; i < 6; i++){
-      document.getElementById(`color${i}`).addEventListener("click", function(){navigator.clipboard.writeText(hexCodes[i-1])})
-      document.getElementById(`color${i}-name`).addEventListener("click", function(){navigator.clipboard.writeText(hexCodes[i-1])})
+      document.getElementById(`color${i}`).addEventListener("click", function(){navigator.clipboard.writeText(hexCodes[i])})
+      document.getElementById(`color${i}-name`).addEventListener("click", function(){navigator.clipboard.writeText(hexCodes[i])})
     }
+
+    document.getElementById("root-color-name").addEventListener("change", console.log("hello"))
 
     // document.getElementsByClassName("color-area").map()
     // document.getElementById(`color${i}`).addEventListener("click", navigator.clipboard.writeText(value))
